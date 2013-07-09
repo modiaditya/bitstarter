4b030af9f5bb43136f8570e2d9e0ca2c792c45d2
Aditya Modi <aditya.ucla@gmail.com>
2013-07-09 03:42:21 +0000

    changing the code to accept url as parameter

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -40,12 +41,15 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlPage = function(htmlpage) {
+		return cheerio.load(htmlpage);
+};
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+    $ = cheerioHtmlPage(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +65,23 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var restCall = function(url){
+	rest.get(url).on('complete', function(data){
+		var checkJson = checkHtmlFile(data.toString(), program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  
+	});
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+				.option('-u, --url <web_page url>', 'Path to the webpage to be tested')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		var html_response ; 
+		var data = restCall(program.url);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
